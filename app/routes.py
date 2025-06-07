7f2a0b410002e6573fa6539d29d47c98a4cbe954
Quoka-PYX <127728293+Dave-114-P@users.noreply.github.com>
2025-06-08 00:05:02 +0800

    nothing

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,20 @@
 from app import app
-
+from flask import render_template
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello World!"
+    user = {'username':'DD'}
+    # return '''
+    # <html>
+    #     <head>
+    #         <title>Home Page - Microblog</title>
+    #     </head>
+    #     <body>
+    #         <h1>Hello. '''+ user['username']+'''</h1>
+    #     </body>
+    # </html>'''
+    return render_template('index.html', title='HOME',user=user)
+
+@app.route('/login')
+def login():
+    return render_template('login.html', title='LOGIN')
